742d84776b1fd01812c29a2e2dd58a836510de91
Break Yang <BreakDS@gmail.com>
2013-06-30 08:47:29 -0400

    abandoning new search.lisp

--- lisp/search.lisp
+++ lisp/search.lisp
@@ -170,7 +170,10 @@ stuffed-armor"
 ;; 1. search result are represent as combos
 ;; 2. a combo is defined as either one of the following forms
 ;;    - (:armor armor-id &rest jewel-ids)
-;;    - (:combo &rest combos)
+;;    - (:combo armor list)
+;;    - (&rest combos)
+
+
 
 (defun gen-skill-filter (item-list)
   (let ((inverse-map (make-array (length *skills*)
@@ -183,8 +186,8 @@ stuffed-armor"
              do (when (> (cadr skill-descr) 0)
                   (push (carriable-id item) 
                         (aref inverse-map (car skill-descr))))))
-    (lambda (skill-id-list)
-      (remove-duplicates (loop for i in skill-id-list
+    (lambda (skill-list)
+      (remove-duplicates (loop for i in skill-list
                             append (aref inverse-map i))))))
 
 (macrolet ((create-skill-filters (&rest names)
@@ -197,14 +200,160 @@ stuffed-armor"
 
 
 
+(defun gen-encoder (skill-list)
+  #f
+  (let* ((n (length skill-list))
+         (mask 0))
+    (declare (type fixnum n))
+    (declare (type fixnum mask))
+    (loop for i below n
+       do (setf mask (the fixnum 
+                       (+ (ash mask 8) 32))))
+    (lambda (item)
+      #f
+      (let ((code 0))
+        (declare (type fixnum code))
+        (loop for skill-id in skill-list
+           do (setf code (the fixnum 
+                           (+ (ash code 8) 
+                              (+ 32 (or (cadr (find skill-id (carriable-skills item)
+                                                   :key #'car))
+                                        0))))))
+        (the fixnum code)))))
 
 
-       
+
+
+
+
+(defun gen-code-combiner (skill-list)
+  #f
+  (let ((n (length skill-list)))
+    (declare (type fixnum n))
+    (let ((mask 0))
+      (declare (type fixnum mask))
+      (loop for i below n 
+         do (setf mask (the fixnum (+ (ash mask 8) 32))))
+      (lambda (x y)
+        (declare (optimize (speed 3) (safety 0)))
+        (declare (type fixnum x))
+        (declare (type fixnum y))
+        (declare (type fixnum mask))
+        (the fixnum (- (+ x y) mask))))))
+
+
+
+
+
+
+           
+(defun init-jewel-set (combine jewel-cand)
+  #f
+  (let ((jewel-set (make-array 4 
+                               :initial-element nil
+                               :adjustable nil
+                               :fill-pointer nil
+                               :displaced-to nil)))
+    (macrolet ((pure-n-hole (n)
+                 `(remove-if-not (lambda (x)
+                                   (= ,n (carriable-holes (aref *jewels-array* (cadr x)))))
+                                 jewel-cand)))
+      ;; 1 hole
+      (setf (aref jewel-set 1) (pure-n-hole 1))
+      ;; 2 holes and 3 holes
+      (macrolet ((cartesian (a b)
+                   (exmac:with-gensyms (i j)
+                     `(loop for ,i in ,a
+                         append (loop for ,j in ,b
+                                   collect (cons (funcall combine 
+                                                          (car ,i) 
+                                                          (car ,j))
+                                                 (append (cdr ,i)
+                                                         (cdr ,j))))))))
+        ;; 2 holes
+        (setf (aref jewel-set 2) 
+              (append (aref jewel-set 1)
+                      (pure-n-hole 2)
+                      (delete-if (lambda (x) (> (cadr x) (caddr x)))
+                                 (cartesian (aref jewel-set 1)
+                                            (aref jewel-set 1)))))
+        ;; 3 holes
+        (setf (aref jewel-set 3)
+              (append (aref jewel-set 1)
+                      (aref jewel-set 2)
+                      (pure-n-hole 3)
+                      (cartesian (aref jewel-set 1)
+                                 (aref jewel-set 2))))
+        jewel-set))))
+                      
+                      
+          
+                  
+  
+
+                    
+    
+  
   
 
 
-;; (defun search-armor (req-set &key (weapon 'both) (mask nil))
-;;   "main search function"
+(defun search-armor (req-set)
+  "main search function"
+  (let* ((skill-list (mapcar #`,(car x1) req-set))
+         (encode (gen-encoder skill-list))
+         (combine (gen-code-combiner skill-list))
+         (jewel-cand (mapcar #`(,(funcall encode (aref *jewels-array* x1))
+                                 ,x1)
+                             (filter-jewels skill-list)))
+         (jewel-set (init-jewel-set combine jewel-cand)))
+    (macrolet ((prepare-armor-list (part)
+                 (exmac:with-gensyms (item-id jewel-list item item-code)
+                   `(loop for ,item-id in (,(exmac:symb 'filter- part) skill-list)
+                       append (let* ((,item (aref ,(exmac:symb '* part '-array*) ,item-id))
+                                     (,item-code (funcall encode ,item)))
+                                (cons `(,,item-code ,,item-id)
+                                      (loop for ,jewel-list in 
+                                           (aref jewel-set (carriable-holes ,item))
+                                         collect (cons (funcall combine 
+                                                                ,item-code
+                                                                (car ,jewel-list))
+                                                       (cadr ,jewel-list))))))))
+               (cascade (part from)
+                 (exmac:with-gensyms (every res candidates)
+                   `(let ((,res (make-hash-table :test #'eq))
+                          (,candidates (prepare-armor-list ,part)))
+                      (maphash (lambda (code lst)
+                                 (loop for ,every in ,candidates
+                                    do (let ((new-code (funcall combine 
+                                                                code 
+                                                                (car ,every))))
+                                         (push (list :combo
+                                                     (cdr ,every)
+                                                     lst)
+                                               (gethash new-code ,res)))))
+                               ,from)
+                      ,res))))
+      (cascade helms 
+               (cascade chests
+                        (cascade gloves
+                                 (cascade belts
+                                          (let ((res (make-hash-table :test #'eq)))
+                                            (loop for every in (prepare-armor-list boots)
+                                               do (push (cdr every)
+                                                        (gethash (car every) res)))
+                                            res))))))))
+                                             
+                                         
+                   
+
+      
+    
+                 
+                                      
+
+                                      
+                                      
+  
   
   
          
